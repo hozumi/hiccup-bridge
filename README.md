60709b8f0ccbf4c31af2859e7dda14ccfccc2f9b
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-10-29 23:09:27 +0900

    fix README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hiccup-bridge
+
+Hiccup-bridge is a conversion tool between the functions including hiccup vector and html.
+
+## Usage
+
+    % lein hicv 2html
+The functions which include hiccup vector are gathered and converted to html files under "hicv" directory.
+    
+    % lein hicv 2hic
+Htmls under "hicv" directory are conveted to hiccup node and pprinted into terminal.
+    
+## Installation
+
+Leiningen
+    :dev-dependencies [[org.clojars.hozumi/hiccup-bridge "1.0.0-SNAPSHOT"]]
+
--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject org.clojars.hozumi/hiccup-bridge "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Hiccup to html, html to hiccup."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.clojars.hozumi/clj-glob "0.1.0"]
 		 [pattern-match "1.0.0"]
 		 [hiccup "0.3.0"]
 		 [enlive   "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-clojars "0.6.0"]])
