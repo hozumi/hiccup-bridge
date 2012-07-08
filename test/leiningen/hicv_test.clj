a698e5f6c1eba80ae37b6674d5f4a8536b3e838b
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2012-07-08 20:26:46 +0900

    add test

---
+++ test/leiningen/hicv_test.clj
@@ -0,0 +1,24 @@
+(ns leiningen.hicv-test
+  (:use clojure.test)
+  (:require [leiningen.hicv :as hicv]))
+
+(deftest replace-extension-test
+  (is (= "hicv/aaa.html"
+         (#'hicv/replace-extension "hicv/aaa.clj" ".html")))
+  (is (= "hicv/aaa/bbb.clj"
+         (#'hicv/replace-extension "hicv/aaa/bbb.html" ".clj"))))
+
+(deftest id&class-tag-test
+  (is (= :img (#'hicv/id&class-tag "img" {})))
+  (is (= :img.myclass (#'hicv/id&class-tag "img" {:class "myclass"})))
+  (is (= :img#myid (#'hicv/id&class-tag "img" {:id "myid"})))
+  (is (= :img#myid.myclass (#'hicv/id&class-tag "img" {:id "myid" :class "myclass"})))
+  (is (= :img#myid.myclass1.myclass2
+         (#'hicv/id&class-tag "img" {:id "myid" :class "myclass1 myclass2"}))))
+
+(deftest ensure-under-hicv-dir-test
+  (is (= "hicv/aaa.html" (#'hicv/ensure-under-hicv-dir "hicv/aaa.html")))
+  (is (= "hicv/bbb.html" (#'hicv/ensure-under-hicv-dir "/aaa/bbb.html")))
+  (is (= "hicv/http:__google.com_aaa"
+         (#'hicv/ensure-under-hicv-dir "http://google.com/aaa"))))
+
