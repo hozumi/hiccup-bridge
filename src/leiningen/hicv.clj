2ff1adb3e9869eb690b29a279825365cf902ec38
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-09-17 00:20:32 +0700

    move hicv back to leiningen

---
+++ src/leiningen/hicv.clj
@@ -0,0 +1,12 @@
+(ns leiningen.hicv
+  (:use to-hiccup.core))
+
+(defn ^:no-project-needed hicv
+  "Convert html into hiccup and vice verse"
+  [project & [first-arg & rest-args]]
+  (condp = first-arg
+    "2html" (hiccup-files->html-files rest-args)
+    "2clj"  (html-files->hiccup-files rest-args)
+    (println "Usage:
+  lein hicv 2html
+  lein hicv 2clj\n")))
--- src/to_hiccup/core.clj
+++ src/to_hiccup/core.clj
@@ -107,13 +107,3 @@
                          resource-paths)]
     (dorun
      (map html-file->hiccup-file resource-paths))))
-
-(defn ^:no-project-needed hicv
-  "Convert html into hiccup and vice verse"
-  [project & [first-arg & rest-args]]
-  (condp = first-arg
-    "2html" (hiccup-files->html-files rest-args)
-    "2clj"  (html-files->hiccup-files rest-args)
-    (println "Usage:
-  lein hicv 2html
-  lein hicv 2clj\n")))
