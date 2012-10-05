f149312a1f4995a70603134d4d1c5666c5ddfa01
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2012-10-06 04:11:30 +0900

    change :use -> :require

--- src/leiningen/hicv.clj
+++ src/leiningen/hicv.clj
@@ -1,12 +1,12 @@
 (ns leiningen.hicv
-  (:use hiccup-bridge.core))
+  (:require [hiccup-bridge.core :as hic]))
 
 (defn ^:no-project-needed hicv
   "Convert html into hiccup and vice verse"
   [project & [first-arg & rest-args]]
   (condp = first-arg
-    "2html" (hiccup-files->html-files rest-args)
-    "2clj"  (html-files->hiccup-files rest-args)
+    "2html" (hic/hiccup-files->html-files rest-args)
+    "2clj"  (hic/html-files->hiccup-files rest-args)
     (println "Usage:
   lein hicv 2html
   lein hicv 2clj\n")))
